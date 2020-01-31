6d8485e15330fa6e5b5b9dbe4c9ae3025a1647c4
Lemon-cmd <baopham4998@gmail.com>
2020-01-31 02:00:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 int main()
 {
     pD parse = pD();
-    parse.load("data/household_power_consumption.txt");
+    parse.load("data/seeds_dataset.txt");
     
     KMC kmc = KMC();
-    vector <int> ignores = {0,1}; //ignore  columns
+    //vector <int> ignores = {0,1}; //ignore  columns
 
     kmc.setData(parse.getData());
-    kmc.clean(ignores);
+    //kmc.clean(ignores);
     kmc.cluster(5, 10);
 
 }
